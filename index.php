25f04947837e121a22e0d1f39111d18a549652af
Witys112 <w.gasiewski@o2.pl>
2023-09-27 18:52:20 +0200

    1 day
    1day

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="font.css">
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+    <title>welcome!</title>
+</head>
+<body>
+    <div class="choice">
+
+        <h2>siemano</h2>
+        <br><br><br><br><br><br><br><br>
+            
+        <h2>Login:</h2>
+        <form method="post" action="login.php">
+            <input type="submit" value="Login!">
+        </form>
+
+        <h2>Register:</h2>
+        <form method="post" action="register.php">
+            <input type="submit" value="Register!">
+        </form>
+
+    </div>
+
+
+
+</body>
+</html>
